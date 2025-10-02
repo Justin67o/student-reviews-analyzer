68861312f0ca4186a7b41a55a12bce8e2ca18f01
Justin67o <jg3chen@uwaterloo.ca>
2025-10-02 17:30:40 -0400

    Set up basic web app with streamlit Utilized the streamlit python library to create a basic web page to host my sentiment-analysis ai

---
+++ app.py
@@ -0,0 +1,26 @@
+from transformers import pipeline
+import streamlit as st
+
+classifier = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment")
+print(classifier("I am very happy!"))
+
+st.title("Student Reviews Sentiment Analyzer")
+st.write("Enter a student review and see if it's positive, neutral, or negative")
+
+user_input = st.text_area("Type review here")
+
+label_map = {
+    "LABEL_0": "Negative",
+    "Label_1": "Neutral",
+    "Label_2": "Positive"
+}
+
+if st.button("Analyze Sentiment"):
+    if user_input.strip() != "":
+        result = classifier(user_input)[0]
+        label = result['label']
+        score = result['score']
+        st.write(f"**Sentiment:** {label}")
+        st.write(f"**Confidence** {score:.2f}")
+    else:
+        st.write("Please enter some text to analyze.")
